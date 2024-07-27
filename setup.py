d05bdb679549647446e6c2bc838a5932cc72369e
JGWill <jgi@jgwill.com>
2024-07-26 22:44:17 -0400

    add:save_as_json

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jgcmlib',
-    version='1.0.0',
+    version='1.0.1',
     author='J.Guillaume D-Isabelle',
     description='A Python module jgcmlib',
     url='https://github.com/jgwill/jgcmlib',
