7d5f41e612ca9d0db42d6d7cf35057be617ca74a
JGWill <jgi@jgwill.com>
2024-07-29 00:35:37 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jgcmlib',
-    version='1.0.30',
+    version='1.0.31',
     author='J.Guillaume D-Isabelle',
     description='A Python module jgcmlib',
     url='https://github.com/jgwill/jgcmlib',
